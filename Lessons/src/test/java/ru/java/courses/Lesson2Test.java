69f118dacc4e3a964d60a0e35f2a9e2a20a54aa0
PavelGubanov_HP <pavelgubanov2001@mail.ru>
2019-12-09 22:13:06 +0300

    Запустил IDEA на ноутбуке HP

--- Lessons/src/test/java/ru/java/courses/Lesson2Test.java
+++ Lessons/src/test/java/ru/java/courses/Lesson2Test.java
@@ -1,8 +1,8 @@
 package ru.java.courses;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
